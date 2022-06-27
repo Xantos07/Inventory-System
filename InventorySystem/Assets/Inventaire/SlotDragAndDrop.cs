7b8def3bd8311c5c167e5fd9c5fe6641a3176ed5
Xantos07 <coalstudio7@gmail.com>
2022-06-27 20:54:10 +0200

    Item Slipt

--- InventorySystem/Assets/Inventaire/SlotDragAndDrop.cs
+++ InventorySystem/Assets/Inventaire/SlotDragAndDrop.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
-public class SlotDragAndDrop : MonoBehaviour,IPointerClickHandler
+public class SlotDragAndDrop : MonoBehaviour,IPointerClickHandler, IPointerEnterHandler
 {
     [SerializeField] private Canvas canvas;
     private RectTransform rectTransform;
@@ -15,8 +16,10 @@ public class SlotDragAndDrop : MonoBehaviour,IPointerClickHandler
 
     public List<InventoryItem> slotDivisionCount;
     
+    private PointerEventData eventDataClick;
     private PointerEventData eventDataEnter;
     private bool isDragging = false;
+    private int itemCount = 0;
     
     private void Start()
     {
@@ -28,16 +31,20 @@ public class SlotDragAndDrop : MonoBehaviour,IPointerClickHandler
     private void Update()
     {
         if (!isDragging)
-            return;
-        
+        {
+            slotDivisionCount.Clear();
+            itemCount = 0;
+            return;   
+        }
+
         transform.position = Input.mousePosition;
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (eventDataEnter.pointerClick != null &&eventDataEnter.pointerClick.GetComponent<SlotDragAndDrop>() != null)
+            if (eventDataClick.pointerClick != null &&eventDataClick.pointerClick.GetComponent<SlotDragAndDrop>() != null)
             {
-                GetComponent<RectTransform>().anchoredPosition = eventDataEnter.pointerClick.GetComponent<SlotDragAndDrop>().GetOrinalPostion();
-                eventDataEnter.pointerClick.GetComponent<SlotDragAndDrop>().SetOrinalPostion(orinalPostion);
+                GetComponent<RectTransform>().anchoredPosition = eventDataClick.pointerClick.GetComponent<SlotDragAndDrop>().GetOrinalPostion();
+                eventDataClick.pointerClick.GetComponent<SlotDragAndDrop>().SetOrinalPostion(orinalPostion);
             }
             else
             {
@@ -50,12 +57,61 @@ public class SlotDragAndDrop : MonoBehaviour,IPointerClickHandler
         
         if (Input.GetMouseButton(1))
         {
-Debug.Log("tu es entrain de diviser");
+            Debug.Log("tu es entrain de diviser");
+            if (eventDataEnter.pointerEnter != null && 
+                eventDataEnter.pointerEnter.GetComponent<InventoryItem>() != null
+                && inventoryItem.GetItem() !=null && inventoryItem.GetAmountItem() != 1)
+            {
+                InventoryItem slot = eventDataEnter.pointerEnter.GetComponent<InventoryItem>();
+
+                if (!slotDivisionCount.Contains(inventoryItem))
+                {
+                    slotDivisionCount.Add(inventoryItem);
+                }
+                
+                if (!slotDivisionCount.Contains(slot))
+                {
+                    slotDivisionCount.Add(slot);
+                    Split(slot);
+                }
+            }
         }
+    }
+
+    void Split(InventoryItem _slot)
+    {
+        _slot.SetItem(inventoryItem.GetItem());
+
+        int num = itemCount / (slotDivisionCount.Count );
+        int numFinal= 0;
+
+        foreach (InventoryItem slotDivision in slotDivisionCount)
+        {
+            slotDivision.SliptItem(inventoryItem.GetImageItem().sprite, num);
+            Debug.LogWarning("Tu passes au suicant");
+            numFinal += num;
+        } 
+
+        Debug.LogWarning($"num : {num} / numFinal : {numFinal}");
+        
+        if (numFinal != itemCount)
+        {
+            int extra = itemCount - numFinal;
+            Debug.LogWarning($"extra {extra}");
+            for (int i = 0; i < extra; i++)
+            {
+                slotDivisionCount[i].AddItem(inventoryItem.GetImageItem().sprite, 1);
+            }
+        }
+
+        numFinal = 0;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        itemCount = inventoryItem.GetAmountItem();
+        Debug.LogWarning(itemCount);
+        
         orinalPostion = rectTransform.anchoredPosition;
 
         if(eventData.pointerClick.GetComponent<SlotDragAndDrop>() == null)
@@ -64,8 +120,14 @@ Debug.Log("tu es entrain de diviser");
         canvasGroup.blocksRaycasts = isDragging;
         isDragging = !isDragging;
         
+        eventDataClick = eventData;
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
         eventDataEnter = eventData;
     }
+    
     public void SetOrinalPostion(Vector2 _newPostion)
     {
         orinalPostion = _newPostion;
